48f358ccec445d96aa35e08dcb516c60dc0582c2
Antcating <yevhenii.fedorchenko@gmail.com>
2023-11-08 13:09:41 +0200

    refactor: :art: Separate concat module

---
+++ src/concat/hdf.py
@@ -0,0 +1,113 @@
+from datetime import datetime
+import pytz
+import h5py
+from os.path import join
+
+from config import PATH, DATA_LOSE_THRESHOLD, TIME_SAMPLES, SPACE_SAMPLES
+from log.logger import compose_log_message, set_logger
+
+log = set_logger("HDF_PROCESSING", global_concat_log=True)
+
+
+class H5_FILE:
+    """Main class for h5 processing"""
+
+    def __init__(self, file_dir: str, file_name: str) -> None:
+        try:
+            self.file = h5py.File(
+                join(PATH, file_dir, file_name), "r", rdcc_nbytes=2 * 1024 * 4
+            )
+        except OSError as err:
+            raise Exception(f"{file_dir + '/' + file_name} file is corrupted:", err)
+
+        self.file_name: str = file_name
+        self.file_dir: str = file_dir
+
+        # Values to be calculated
+        self.packet_time: float = None
+        self.packet_datetime: datetime = None
+        self.dset: h5py.Dataset = None
+        # Used if needed
+        self.is_chunk_end = False
+        self.is_day_end = False
+        self.dset_split: h5py.Dataset = None
+        self.dset_carry: h5py.Dataset = None
+
+        self.unpack_stat = True
+
+        self.unpack_h5_data()
+
+    def unpack_h5_data(self) -> None:
+        """Unpacks at and data from h5"""
+        try:
+            self.packet_time = float(
+                self.file_name.split("_")[-1].rsplit(".", maxsplit=1)[0]
+            )
+            self.packet_datetime = datetime.fromtimestamp(self.packet_time, tz=pytz.UTC)
+        except ValueError:
+            log.error(
+                compose_log_message(
+                    working_dir=self.file_dir,
+                    file=self.file_name,
+                    message="Unable to read file's packet_time, unable to use it",
+                )
+            )
+            self.unpack_stat = False
+
+        try:
+            self.dset = self.file["data_down"]
+        except KeyError:
+            log.error(
+                compose_log_message(
+                    working_dir=self.file_dir,
+                    file=self.file_name,
+                    message="Unable to unpack data from h5 file, dataset missing",
+                )
+            )
+            self.unpack_stat = False
+
+    def check_h5(self, last_timestamp: float) -> bool:
+        """Checks h5 file if it fits the requirements to be concatenated
+
+                Args:
+                    last_timestamp (float): Timestamp from previous file.
+        Used to calculate time difference between adjoint packets and find lost packets
+
+                Returns:
+                    bool: Status boolean. True if everything OK, False otherwise
+        """
+        try:
+            # DATA lost warning
+            packet_diff = self.packet_time - last_timestamp
+            if last_timestamp != 0 and packet_diff > DATA_LOSE_THRESHOLD:
+                log.warning(
+                    compose_log_message(
+                        working_dir=self.file_dir,
+                        file=self.file_name,
+                        message=f"\
+Possibly missing data: {last_timestamp+4}->{self.packet_time-2}",
+                    )
+                )
+                self.unpack_stat = False
+                return self.unpack_stat, "missing"
+
+            # Shape check
+            if (
+                self.dset.shape[0] != TIME_SAMPLES
+                or self.dset.shape[1] != SPACE_SAMPLES
+            ):
+                log.warning(
+                    compose_log_message(
+                        working_dir=self.file_dir,
+                        file=self.file_name,
+                        message=f"\
+Packet {self.file_name} has unexpected shape: {self.dset.shape}",
+                    )
+                )
+                self.unpack_stat = False
+                return self.unpack_stat, "downsampling"
+
+            return self.unpack_stat, None
+        except AttributeError:
+            self.unpack_stat = False
+            return self.unpack_stat, "key"
---
+++ src/concat/status.py
@@ -0,0 +1,274 @@
+import os
+import json
+
+# import deal
+import shutil
+import datetime
+
+from config import PATH, SAVE_PATH
+
+from log.logger import compose_log_message, set_logger
+
+log = set_logger("STATUS", global_concat_log=True)
+
+
+# @deal.post(lambda x: type(x) is list)
+def get_dirs(filedir_r: str, today: bool = False) -> list:
+    """Returns dirs in path except dir named by today's date in format YYYYMMDD
+
+    Args:
+        absolute_path (str): path to dir to scan to dirs
+
+    Returns:
+        list: list of dirs in the path (+today). Format YYYYMMDD
+    """
+    filedir_abs: str = os.path.join(PATH, filedir_r)
+    dirs: list[str] = []
+    if os.path.isdir(filedir_abs):
+        log.debug(
+            compose_log_message(working_dir=filedir_r, message="Reading directory")
+        )
+        if today:
+            dirs = sorted(
+                [
+                    dir
+                    for dir in os.listdir(filedir_abs)
+                    if os.path.isdir(os.path.join(filedir_abs, dir))
+                ]
+            )
+        else:
+            today_datetime: datetime.datetime = datetime.datetime.now(tz=datetime.UTC)
+            today_formatted: str = datetime.datetime.strftime(today_datetime, "%Y%m%d")
+
+            dirs = sorted(
+                [
+                    dir
+                    for dir in os.listdir(filedir_abs)
+                    if os.path.isdir(os.path.join(filedir_abs, dir))
+                    and dir != today_formatted
+                ]
+            )
+    else:
+        log.warning(
+            compose_log_message(
+                working_dir=filedir_r, message="Directory does not exists"
+            )
+        )
+    return dirs
+
+
+# @deal.post(lambda x: type(x) is list)
+def get_h5_files(
+    dir_path_r: str, last_filename: str | None = None, limit: int = None
+) -> list[str]:
+    """Returns h5 files for processing
+
+    Args:
+        path (str): Path to directory with h5 files
+        last_file (str | None, optional): last processed file. Defaults to None.
+
+    Returns:
+        list: List relevant of h5 files in directory
+    """
+    try:
+        file_names: list[str] = sorted(
+            [
+                os.path.join(PATH, dir_path_r, name)
+                for name in os.listdir(os.path.join(PATH, dir_path_r))
+                if name.endswith(".h5")
+            ]
+        )
+        if last_filename is not None:
+            try:
+                file_names = file_names[file_names.index(last_filename) + 1 :]
+            except ValueError:
+                log.warning("File was not found in dir during indexing last filename")
+
+        return sorted(file_names[:limit])
+    except FileNotFoundError:
+        log.warning(
+            compose_log_message(
+                working_dir=dir_path_r, message="Directory does not exist"
+            )
+        )
+        return []
+    except ValueError:
+        return []
+
+
+# @deal.has("write")
+def save_status(
+    filedir_r: str,
+    last_filename: str,
+    last_filedir_r: str,
+    start_chunk_time: float,
+    processed_time: int,
+):
+    """Writes last processed file's name, total_unit_size and start_chunk_time
+    to {date}/.last
+
+    Args:
+        working_path_abs (str): absolute PATH to working dir
+        last_filename (str): last processed file's name
+        start_chunk_time (float): time of the beginning of the first chunk
+        processed_time (int): size of the last chunk including file's data
+    """
+    status_vars = json.dumps(
+        {
+            "last_filename": last_filename,
+            "last_filedir": last_filedir_r,
+            "start_chunk_time": start_chunk_time,
+            "processed_time": processed_time,
+        }
+    )
+
+    with open(
+        os.path.join(PATH, filedir_r, ".last"), "w", encoding="UTF-8"
+    ) as status_file:
+        status_file.write(status_vars)
+    if filedir_r != last_filedir_r:
+        status_vars_n = json.dumps(
+            {
+                "last_filename": last_filename,
+                "last_filedir": last_filedir_r,
+            }
+        )
+
+        with open(
+            os.path.join(PATH, last_filedir_r, ".last"), "w", encoding="UTF-8"
+        ) as status_file:
+            status_file.write(status_vars_n)
+
+
+# @deal.post(lambda *args: type(*args) is tuple)
+# @deal.pre(lambda path_abs: type(path_abs) is str)
+def get_queue(filepath_r: str):
+    """Calculates files that are left to process in directory
+    Also calculates several necessary vars to proceed with concat
+
+    Args:
+        absolute_path (str): absolute PATH to the working dir
+    """
+
+    def set_defaults(last_filename: str = None) -> tuple:
+        h5_files_list: list[str] = get_h5_files(
+            dir_path_r=filepath_r, last_filename=last_filename
+        )
+
+        if h5_files_list:
+            start_chunk_time: float = float(
+                h5_files_list[0].split("_")[-1].rsplit(".", 1)[0]
+            )
+        else:
+            start_chunk_time = 0
+
+        processed_time: int = 0
+        last_timestamp: int = 0
+        return (
+            h5_files_list,
+            start_chunk_time,
+            processed_time,
+            last_timestamp,
+        )
+
+    if os.path.isfile(os.path.join(PATH, filepath_r, ".last")):
+        with open(
+            os.path.join(PATH, filepath_r, ".last"), "r", encoding="UTF-8"
+        ) as status_file:
+            status_vars = json.load(status_file)
+            last_filename = status_vars["last_filename"]
+            last_filedir_r = status_vars["last_filedir"]
+
+            if "start_chunk_time" in status_vars:
+                start_chunk_time = status_vars["start_chunk_time"]
+                processed_time = status_vars["processed_time"]
+
+                file_names_tbd = get_h5_files(
+                    dir_path_r=last_filedir_r,
+                    last_filename=last_filename,
+                )
+                last_timestamp = float(
+                    last_filename.split("_")[-1].rsplit(".", maxsplit=1)[0]
+                )
+                return (
+                    file_names_tbd,
+                    start_chunk_time,
+                    processed_time,
+                    last_timestamp,
+                )
+            else:
+                return set_defaults(last_filename=last_filename)
+    else:
+        return set_defaults()
+
+
+# @deal.post(lambda x: x is True)
+def reset_chunks(file_dir_r: str) -> bool:
+    """Deletes start_chunk_time and total_unit_size
+    Used upon error (gap) in data to proceed after without exiting
+
+    Args:
+        path_dir (str): absolute PATH to working dir
+    """
+
+    log.info(
+        compose_log_message(message="Reset chunk tracking to start from new chunk")
+    )
+    status_filepath_r = os.path.join(PATH, file_dir_r, ".last")
+    if os.path.isfile(status_filepath_r):
+        with open(status_filepath_r, "r", encoding="UTF-8") as status_file:
+            status_vars: dict = json.load(status_file)
+
+        status_vars.pop("start_chunk_time", None)
+        status_vars.pop("processed_time", None)
+
+        with open(status_filepath_r, "w", encoding="UTF-8") as status_file:
+            status_vars: dict = json.dump(status_vars, status_file)
+    return True
+
+
+def track_to_be_deleted(filepath: str):
+    with open(
+        os.path.join(SAVE_PATH, ".to_be_deleted"), "a+", encoding="UTF-8"
+    ) as track_deleted:
+        track_deleted.write(os.path.join(filepath) + ";")
+
+
+def delete_processed_files():
+    log.info(compose_log_message(message="Deleting h5 files upon saving the chunk"))
+    if os.path.isfile(os.path.join(SAVE_PATH, ".to_be_deleted")):
+        with open(
+            os.path.join(SAVE_PATH, ".to_be_deleted"), "r", encoding="UTF-8"
+        ) as to_be_deleted:
+            files_paths = to_be_deleted.read()
+            files_paths_list = files_paths.split(";")
+            for filepath_abs in files_paths_list:
+                if filepath_abs:
+                    try:
+                        os.remove(filepath_abs)
+                    except FileNotFoundError:
+                        log.warning(
+                            compose_log_message(
+                                message="During deleting file an error ocurred"
+                            )
+                        )
+        os.remove(os.path.join(SAVE_PATH, ".to_be_deleted"))
+
+
+def preserve_last_processed():
+    log.info(
+        compose_log_message(
+            message="Preserving h5 files to investigate unexpected error"
+        )
+    )
+    if os.path.isfile(os.path.join(SAVE_PATH, ".to_be_deleted")):
+        os.remove(os.path.join(SAVE_PATH, ".to_be_deleted"))
+
+
+def delete_dirs():
+    log.info(
+        compose_log_message(message="Deleting directories without h5 files in them")
+    )
+    for filedir_r in get_dirs(PATH):
+        if not get_h5_files(filedir_r):
+            shutil.rmtree(os.path.join(PATH, filedir_r))
