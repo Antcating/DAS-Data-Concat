8b495f1f97efb7fb54cd07474f13ef186d7cb238
Antcating <yevhenii.fedorchenko@gmail.com>
2023-11-11 17:55:20 +0200

    feat: :zap: Telegram log error implemented

--- src/concat.py
+++ src/concat.py
@@ -1,14 +1,18 @@
 from concat.main import main
 from log.telegram import send_telegram_error
+from log.logger import set_logger, set_console_logger
 
 import traceback
 
+log = set_logger("MAIN", global_concat_log=True)
+set_console_logger(log, log_level="ERROR")
 
 try:
     main()
 except Exception:
     msg = (
-        "Error occurred during scheduled concatenation. Details:\n\n"
+        "Unexpected error occurred during scheduled concatenation. Details:\n\n"
         + traceback.format_exc()
     )
     send_telegram_error(msg)
+    log.exception(msg)
